af4bee5b592758dce87e2dfba14c8e6482d13ebd
Sharmeen <sharmeenk2020@gmail.com>
2026-05-29 16:00:00 +0530

    Add chrome local storage to check for existing videos ids

--- scripts/content.js
+++ scripts/content.js
@@ -3,13 +3,46 @@
     if(video){
         console.log("I FOUND THE VIDEO")
     }
+    const urlParam=new URLSearchParams(window.location.search)
+    const videoId=urlParam.get('v')
+    if(!videoId)return;
+
+    //check for existing url : time pair
+    chrome.storage.local.get([videoId],(result)=>{
+        if(result[videoId]){
+            video.currentTime=result[videoId]
+        }
+    })
     video.addEventListener("playing",(event)=>{
         console.log("THIS VIDEO IS MOW PLAYING")
     })
-    const dur=video.duration
-    console.log("DURATION: "+dur) 
-    video.fastSeek(20); 
-    console.log("I AM WORKing")
+
+    //run every 3 seconds while the video plays
+    setInterval(()=>{
+        if(!video.paused && !video.currentTime>2){
+            chrome.storage.local.set({[videoId]:video.currentTime})
+        }
+    },3000)
+
+    /*
+    detect the video element
+    extract the url? store as a key in local storage
+    check if that url is already present
+    if yes -> {
+    retrieve the duration of the url
+    change video duration using seek
+    }
+
+    if not -> create a key value pair of the url and duration (research more)
+    add an event listener to detect duration changes
+    add a timeout so it does occur immediately
+    maybe every 5 seconds
+    update duration accordingly
+
+    Edge Cases: 
+    buffers?
+    seek?
+    */
 }
 
 lastSeenTill(document.querySelector('video'))
@@ -24,6 +57,6 @@ const observer=new MutationObserver((mutations)=>{
     }
 })
 
-// observer.observe(document.querySelector('devsite-content'), {
-//   childList: true
-// });
+observer.observe(document.body, { childList: true, subtree: true });
+
+ 
