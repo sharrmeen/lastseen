9e78b6d617dba85ee576b373742c5c3ceae92a0e
Sharmeen <sharmeenk2020@gmail.com>
2026-06-01 18:40:00 +0530

    Handle unloaded videos with metadata event

--- scripts/content.js
+++ scripts/content.js
@@ -5,44 +5,32 @@
     }
     const urlParam=new URLSearchParams(window.location.search)
     const videoId=urlParam.get('v')
+    console.log(videoId)
     if(!videoId)return;
 
     //check for existing url : time pair
     chrome.storage.local.get([videoId],(result)=>{
         if(result[videoId]){
-            video.currentTime=result[videoId]
+            console.log(result[videoId])
+            const savedTime=result[videoId]
+            if(video.readyState>=1){
+                video.currentTime=savedTime
+            }
+            else{
+                video.addEventListener("loadedmetadata",()=>{
+                    video.currentTime=savedTime
+                },{once:true})
+            }
         }
-    })
-    video.addEventListener("playing",(event)=>{
-        console.log("THIS VIDEO IS MOW PLAYING")
     })
 
     //run every 3 seconds while the video plays
     setInterval(()=>{
-        if(!video.paused && !video.currentTime>2){
+        if(!video.paused && video.currentTime>5){
             chrome.storage.local.set({[videoId]:video.currentTime})
+            console.log("Saved time for video "+videoId+" as "+video.currentTime)
         }
     },3000)
-
-    /*
-    detect the video element
-    extract the url? store as a key in local storage
-    check if that url is already present
-    if yes -> {
-    retrieve the duration of the url
-    change video duration using seek
-    }
-
-    if not -> create a key value pair of the url and duration (research more)
-    add an event listener to detect duration changes
-    add a timeout so it does occur immediately
-    maybe every 5 seconds
-    update duration accordingly
-
-    Edge Cases: 
-    buffers?
-    seek?
-    */
 }
 
 lastSeenTill(document.querySelector('video'))
